f9d643c63c22ddc4ab9807232490526343d7776b
xLychee <xun.luan.rice@gmail.com>
2017-10-22 20:29:01 -0500

    Change to 1-step q

--- ProcessAgent.py
+++ ProcessAgent.py
@@ -187,7 +187,7 @@ class ProcessAgent(Process):
             #experiences.append(exp)
             experience_queue.append(exp)
             updated_exps += ProcessAgent._accumulate_rewards(experience_queue, self.discount_factor, done)
-
+            print(len(updated_exps))
             if (done or time_count == Config.TIME_MAX) and updated_exps:
                 #terminal_reward = 0 if done else value
                 #updated_exps = ProcessAgent._accumulate_rewards(experiences, self.discount_factor, terminal_reward)
